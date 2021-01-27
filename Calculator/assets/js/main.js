f7558003848d0be78fd3ba8d0854b4d34737f97e
Mathewos Worku <se.mathewos.worku@gmail.com>
2021-01-27 12:31:33 +0300

    Loop was added.

--- Calculator/assets/js/main.js
+++ Calculator/assets/js/main.js
@@ -40,44 +40,49 @@ function division(fn,sn)
 
 (function()
 {
-    operation = prompt("Enter the number for the operation you need  1 for addition 2 for subtraction 3 for multiplication 4 for division")
-    if(operation == 1){
-        noOfValues = prompt("how many Numbers do you wanna operate? ");
-        for(let index = 0; index < parseInt(noOfValues); index++){
-        Number[index] = prompt("No of elements" + (index + 1));
-    }
-    }
-    else if(operation == 3) {
-        noOfValues = prompt("how many Numbers do you wanna operate? ");
-        for(let index = 0; index < parseInt(noOfValues); index++){
-        Number[index] = prompt("No of elements" + (index + 1));
-    }
-    }
-    else{
-        firstNumber = prompt("Enter first number: ")
-        secondNumber = prompt("Enter second number: ")
-        tempFN = parseFloat(firstNumber)
-        tempSN = parseFloat(secondNumber)
-    }
+    while (true){
+        operation = prompt("Enter the number for the operation you need  1 for addition 2 for subtraction 3 for multiplication 4 for division")
+        if (operation == 'q') {
+            break;
+        }
+        else if(operation == 1){
+            noOfValues = prompt("how many Numbers do you wanna operate? ");
+            for(let index = 0; index < parseInt(noOfValues); index++){
+            Number[index] = prompt("No of elements" + (index + 1));
+        }
+        }
+        else if(operation == 3) {
+            noOfValues = prompt("how many Numbers do you wanna operate? ");
+            for(let index = 0; index < parseInt(noOfValues); index++){
+            Number[index] = prompt("No of elements" + (index + 1));
+        }
+        }
+        else{
+            firstNumber = prompt("Enter first number: ")
+            secondNumber = prompt("Enter second number: ")
+            tempFN = parseFloat(firstNumber)
+            tempSN = parseFloat(secondNumber)
+        }
     
 
-    if(operation == 1){
-        console.log("Your result" + " " + addition(Number));
-    }    
-    else if (operation == 2){
-        console.log("Your result" + " " + subtraction(tempFN , tempSN));
-    }    
-    else if (operation == 3){
-        console.log("Your result" + " " + " " + multiplication(Number));
-    }
-    else if(operation == 4)
-    {
-        console.log("Your result" + " " +division(tempFN , tempSN));
-    }
-    else
-    {
-        console.log("No operation selected.");
+        if(operation == 1){
+            console.log("Your result" + " " + addition(Number));
+        }    
+        else if (operation == 2){
+            console.log("Your result" + " " + subtraction(tempFN , tempSN));
+        }    
+        else if (operation == 3){
+            console.log("Your result" + " " + " " + multiplication(Number));
+        }
+        else if(operation == 4)
+        {
+            console.log("Your result" + " " +division(tempFN , tempSN));
+        }
+        else
+        {
+            console.log("No operation selected.");
         
+        }
     }
 }
 )();
